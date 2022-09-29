9aa51d1f5230b731a6768549ed21c2aa94a5d68a
Yash Sehgal <yashsehgal@Yashs-MacBook-Air.local>
2022-09-30 00:02:24 +0530

    feat: navbar added

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import HeaderOptions from '../dataSource/header-options.json';
+import LogoImage from '../lib/logo.png';
+
+export default function Header() {
+    const headerOptionsDataRef = useRef(HeaderOptions);
+    return (
+        <div className="header-component flex flex-row items-center justify-between py-4">
+            <span className="name-logo-wrapper">
+                <Link to="/">
+                    <img src={LogoImage} 
+                        alt="logo" 
+                        className="w-[180px] h-auto" />
+                </Link>
+            </span>
+            <span className="header-options-list-wrapper w-fit h-auto">
+                <ul className="header-options-list flex flex-row items-center justify-end gap-5 w-fit h-auto">
+                    {headerOptionsDataRef.current?.map((option, optionIndex) => (
+                        <li className="header-option-item text-sm text-gray-500 hover:text-gray-600" 
+                            key={optionIndex}
+                        >
+                            <Link to={option?.path}>
+                                {option?.title}
+                            </Link>
+                        </li>
+                    ))}
+                    <li className="header-option-item">
+                        <button className="px-3 py-1.5 rounded-full bg-gray-800 hover:bg-gray-700 text-sm text-white"
+                            onClick={() => window.open('https://github.com/hekors')}
+                        >
+                            github
+                        </button>
+                    </li>
+                </ul>
+            </span>
+        </div>
+    )
+}
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+
+export default function Landing() {
+    return (
+        <section className="landing-page-container">
+            <div className="hero-section">
+                
+            </div>
+        </section>
+    )
+}
